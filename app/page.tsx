c509ffb5551a8eea333488ee411bce59f4997285
fabri <fguespe@gmail.com>
2024-04-03 17:31:57 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const frameMetadata = getFrameMetadata({
       target: `https://xmtp.org/dev-call`,
     },
     {
-      label: 'Learn more',
+      label: 'Ask a question',
       action: 'link',
       target: `https://community.xmtp.org/t/tue-apr-9-2024-frames-o-rama-post-a-question-or-topic/629`,
     } /*
